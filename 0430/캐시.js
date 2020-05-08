4f4d2956fbd1085963096796a3bdc60d58f51782
blueboy1593 <snb0303@naver.com>
2020-05-08 18:30:42 +0900

    캐시 js

--- 0430/캐시.js
+++ 0430/캐시.js
@@ -6,22 +6,25 @@ function solution(cacheSize, cities) {
             city = city.toLowerCase()
             // console.log(city)
             if (cach.length < cacheSize){
-                if (city in cach) {
+                if (cach.indexOf(city) > -1) {  // 이거 기가 막히네...
                     answer += 1
+                    // console.log('여기왔다')
                 } else {
                     answer += 5
                 }
                 cach.push(city)
             } else {
-                if (city in cach) {
+                if (cach.indexOf(city) > -1) {
                     answer += 1
                     // cach.remove(city)   // remove가 되나? 안되는거같어 실패작.
                     const idx = cach.indexOf(city)
                     cach.splice(idx, 1) // 이렇게 하라는 거지?
                     cach.push(city)
+                    // console.log('여기왔다')
                 } else {
                     answer += 5
                     cach.shift()
+                    cach.pop(0)
                     // cach.shift() 같은거?
                     cach.push(city)
                 }
