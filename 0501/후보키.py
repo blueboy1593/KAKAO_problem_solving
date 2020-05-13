1fa4ce4e76015157cecd643b14f2217f2d2c1370
blueboy1593 <snb0303@naver.com>
2020-05-13 23:43:07 +0900

    후보키 another way 도전

--- 0501/후보키.py
+++ 0501/후보키.py
@@ -1,6 +1,7 @@
 # 순열조합 문제?? 인지는 모르겠는데, 순열식으로 한번 해보장!
 # 1개짜리는 우선적으로 빼버릴까...?
 # list는 set이 안된다는 거지...
+from copy import deepcopy
 answer = 0
 
 def solution(relation):
@@ -20,16 +21,23 @@ def solution(relation):
 
     # solution 안에 굳이 넣어보는 이유는, global이 어떻게 될 지 궁금해서.
     def find_hubokey(kk, arr):
+        global answer
         for k in range(kk + 1, pre_combs_leng):
             kj = pre_combs[k]
+            tuple_arr = deepcopy(arr)
+            hanging_arr = deepcopy(arr)
             for i in range(row):
-                arr[i].append(relation[i][kj])
-            print(arr)
-            if len(arr) == len(set(arr)):
+                tuple_arr[i].append(relation[i][kj])
+                tuple_arr[i] = tuple(tuple_arr[i])
+                hanging_arr[i].append(relation[i][kj])
+            # append하고 tuple로 만들기까지 한다...
+            # 너무 뻘짓거리 하는거같은데... ㅋㅋㅋ
+            # print(tuple_arr)
+            if row == len(set(tuple_arr)):
                 answer += 1
                 continue
             else:
-                find_hubokey(k + 1, arr)
+                find_hubokey(k, hanging_arr)
 
 
     pre_combs_leng = len(pre_combs)
@@ -39,10 +47,12 @@ def solution(relation):
         kj_col = []
         for i in range(row):
             kj_col.append([relation[i][kj]])
-        print(kj_col)
+        # print(kj_col)
         find_hubokey(k, kj_col)
 
+    # print(answer)
     return answer
 
 
 solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]])
+# 결국 모든 케이스를 구한다음에 contain? 이런 느낌의 함수를 쓰던가! 1, 2, 3, 4, 5 이런식으로 하자
